a89ec10ef86cbd3d6baf149c3cd56daaa9fda532
Michal Latynski <michal@latynski.pl>
2025-06-01 20:09:32 +0200

    Klasa statyczna zapewniające operacje matematyczne dla prostego kalkulatora,
    Przykładowa implementacja wykorzystania prostego kalkulatora poprzez tą klasę w widoku Pracownik
    
    Dokładne wyjaśnienie zaimplementowanego rozwiązania krok po kroku w komenatrzach
    
    Dodatkowo w komentarzach opisałem dlaczego implementacja wygląda w ten sposób dla tego konkretnego przypadku; oraz znajdują się tam propozycje implementacji dla innych, bardziej złożonych przypadków

---
+++ Rekrutacja/Rekrutacja/Helpers/ProstyKalkulatorStaticHelper/ProstyKalkulatorStaticHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekrutacja.Helpers.ProstyKalkulatorStaticHelper
+{
+    /// <summary>
+    /// Statyczna klasa helper do prostego kalkulatora.
+    /// Ze względu na to, że jest to kalkulator prosty, wykonujący tylko 4 uniwersalne operacje (mam na myśli że w przypadku takich wymagań 
+    /// nie widzę tu potrzeby tworzenia instancji klasy - gdyż przy tych najprostszych operacjach tak czy tak nie będziemy w stanie 
+    /// szczególnie tych danych zapisać/analizować
+    /// 
+    /// W przypadku bardziej złożonych operacji, gdzie będziemy chcieli np. obliczać podatek od wynagrodzenia lub inne złożone obliczenia/operacje
+    /// jak najbardziej będzie miało sens tworzenie instancji tych klas, aby móc przykładowo dla obliczania podatku od wynagrodzenia
+    /// będziemy mogli zastosować inny wzór dla instancji klasy, która będzie obliczała podatki dla pracowników z Umową o Pracę,
+    /// inne dla pracowników z Umową Zlecenie, a jeszcze inne dla Umów o Dzieło
+    /// </summary>
+    public static class ProstyKalkulatorStaticHelper
+    {
+        public static int WykonajOperacje(int a, int b, char operacja)
+        {
+            switch (operacja)
+            {
+                case '+':
+                    return a + b;
+                case '-':
+                    return a - b;
+                case '*':
+                    return a * b;
+                case '/':
+                    if (b == 0)
+                        throw new DivideByZeroException("Nie można dzielić przez zero.");
+                    return (int) a / b; // Rzutowanie na int, aby wynik był typu int, ponieważ w przypadku dzielenia może być to liczba zmiennoprzecinkowa; TODO: Sprwdzić w jaki sposób obsługiwać liczby zmiennoprzecinkowe w Enova 365
+                default:
+                    throw new InvalidOperationException($"Nieprawidłowa operacja: {operacja} Dozwolone są: + - * /");
+            }
+        }
+    }
+}
--- Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
+++ Rekrutacja/Rekrutacja/Workers/Template/TemplateWorker.cs
@@ -8,6 +8,14 @@ using Soneta.Kadry;
 using Soneta.KadryPlace;
 using Soneta.Types;
 using Rekrutacja.Workers.Template;
+using Rekrutacja.Helpers.ProstyKalkulatorStaticHelper;
+
+/// Należy utworzyć kalkulator który posiada cztery operacje (+,-,*,/), wynik zostanie zapisany
+/// na WSZYSTKICH ZAZNACZONYCH OBIEKTACH PRACOWNIK w polu Cecha "Wynik" oraz po wykonaniu działania
+/// musi zapisać się data wykonania zadania w polu Cecha "DataObliczen". Aby tego dokonać należy zrobić Worker (przycisk)
+/// który będzie mieć parametry przyjmujące 4 zmienne: 
+/// Zmienna A, Zmienna B, Operacja (znak: +, -, *, /), Data obliczeń.
+
 
 //Rejetracja Workera - Pierwszy TypeOf określa jakiego typu ma być wyświetlany Worker, Drugi parametr wskazuje na jakim Typie obiektów będzie wyświetlany Worker
 [assembly: Worker(typeof(TemplateWorker), typeof(Pracownicy))]
@@ -20,6 +28,12 @@ namespace Rekrutacja.Workers.Template
         {
             [Caption("Data obliczeń")]
             public Date DataObliczen { get; set; }
+            [Caption("A")]
+            public int A { get; set; }
+            [Caption("B")]
+            public int B { get; set; }
+            [Caption("Operacja")]
+            public char Operacja { get; set; }
             public TemplateWorkerParametry(Context context) : base(context)
             {
                 this.DataObliczen = Date.Today;
@@ -43,8 +57,9 @@ namespace Rekrutacja.Workers.Template
         {
             //Włączenie Debug, aby działał należy wygenerować DLL w trybie DEBUG
             DebuggerSession.MarkLineAsBreakPoint();
-            //Pobieranie danych z Contextu
+
             Pracownik pracownik = null;
+
             if (this.Cx.Contains(typeof(Pracownik)))
             {
                 pracownik = (Pracownik)this.Cx[typeof(Pracownik)];
@@ -54,18 +69,43 @@ namespace Rekrutacja.Workers.Template
             //Aby modyfikować dane musimy mieć otwartą sesję, któa nie jest read only
             using (Session nowaSesja = this.Cx.Login.CreateSession(false, false, "ModyfikacjaPracownika"))
             {
-                //Otwieramy Transaction aby można było edytować obiekt z sesji
-                using (ITransaction trans = nowaSesja.Logout(true))
+                try // Zrobię to w bloku try catch, gdyż sama metoda ProstyKalkulatorStaticHelper.WykonajOperacje może rzucić wyjątek, np. w przypadku
+                    // dzielenia przez zero, a chciałbym uniknąć problemów z UI aplikacji Evova 365 lub problemów z samą bazą danych
+                    // przy tak na prawdę pojedynczej prostej operacji
+                    // Oczywiście gdyby było tutaj więcej danych do przetworzenia (a co za tym idzie również do potencjalnego stracenia w przypaku
+                    // wyjątku) to należałoby dodać tutaj więcej logiki, podzielić to na mniejsze operacje - jednak w tym konkretnym Workerze
+                    // nie widzę takiej dużej konieczności, jednak można to rozbudować
                 {
-                    //Pobieramy obiekt z Nowo utworzonej sesji
-                    var pracownikZSesja = nowaSesja.Get(pracownik);
-                    //Features - są to pola rozszerzające obiekty w bazie danych, dzięki czemu nie jestesmy ogarniczeni to kolumn jakie zostały utworzone przez producenta
-                    pracownikZSesja.Features["DataObliczen"] = this.Parametry.DataObliczen;
-                    //Zatwierdzamy zmiany wykonane w sesji
-                    trans.CommitUI();
+                    //Otwieramy Transaction aby można było edytować obiekt z sesji
+                    using (ITransaction trans = nowaSesja.Logout(true))
+                    {
+                        //Pobieramy obiekt z sesji, aby móc go modyfikować
+                        var pracownikZSesja = nowaSesja.Get(pracownik);
+
+
+                        //Features - są to pola rozszerzające obiekty w bazie danych, dzięki czemu nie jestesmy ogarniczeni to kolumn jakie zostały utworzone przez producenta
+
+                        pracownikZSesja.Features["DataObliczen"] = this.Parametry.DataObliczen;
+
+                        // Zastanawiałem się czy blokiem try catch nie objąć tylko tej linii, ale jako że cały Worker robi tak na prawdę jedną operację matematyczną
+                        // to bardziej obawiam się problemów z przechwyceniem wyjątku w trakcie działania UI i otwartej transakcji, niż po prostu
+                        // jej nie zatwierdzając (commitując)
+
+                        pracownikZSesja.Features["Wynik"] = ProstyKalkulatorStaticHelper.WykonajOperacje(this.Parametry.A, this.Parametry.B, this.Parametry.Operacja);
+
+                        //Zatwierdzamy zmiany wykonane w sesji
+                        trans.CommitUI();
+                    }
+
+                    //Zapisujemy zmiany
+                    nowaSesja.Save();
                 }
-                //Zapisujemy zmiany
-                nowaSesja.Save();
+                catch (Exception ex)
+                {
+                    throw new Exception($"Wystąpił błąd");
+                    //TODO: zapis do loga błędu, zgodnie z dobrymi praktykami nie wyświetlamy exceptiona bezpośrednio użytkownikowi
+                }
+
             }
         }
     }
